b669a699a8ac7fb98598df750441868c1517d7d8
TFMLin <99130709@qq.com>
2025-02-13 17:42:28 +0800

    feat: 实现结账功能，整合购物车、订单和支付服务

--- app/checkout/biz/service/checkout.go
+++ app/checkout/biz/service/checkout.go
@@ -2,12 +2,18 @@ package service
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/cloudwego/kitex/pkg/kerrors"
 	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/whlxbd/gomall/app/checkout/infra/rpc"
 
 	"github.com/whlxbd/gomall/common/utils/authpayload"
+	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/cart"
 	checkout "github.com/whlxbd/gomall/rpc_gen/kitex_gen/checkout"
+	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/order"
+	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/payment"
+	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/product"
 )
 
 type CheckoutService struct {
@@ -32,5 +38,79 @@ func (s *CheckoutService) Run(req *checkout.CheckoutReq) (resp *checkout.Checkou
 		return nil, kerrors.NewBizStatusError(401, "user not found")
 	}
 
-	return
+	cartResult, err := rpc.CartClient.GetCart(s.ctx, &cart.GetCartReq{UserId: uint32(payload.UserId)})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cartResult.Cart.Items) == 0 {
+		return nil, kerrors.NewBizStatusError(400, "cart is empty")
+	}
+
+	itemIds := make([]uint32, 0)
+	for _, item := range cartResult.Cart.Items {
+		itemIds = append(itemIds, item.ProductId)
+	}
+
+	productsResult, err := rpc.ProductClient.GetProduct(s.ctx, &product.GetProductReq{Ids: itemIds})
+	if err != nil {
+		return nil, err
+	}
+
+	amount := float32(0)
+	ois := []*order.OrderItem{}
+	for _, pdt := range productsResult.Products {
+		cost := pdt.Price * float32(cartResult.Cart.Items[pdt.Id].Quantity)
+		amount += cost
+		ois = append(ois, &order.OrderItem{
+			Item: &cart.CartItem{ProductId: pdt.Id, Quantity: cartResult.Cart.Items[pdt.Id].Quantity},
+			Cost: cost,
+		})
+	}
+
+	placeOrderReq := order.PlaceOrderReq{
+		UserId:       uint32(payload.UserId),
+		UserCurrency: "CNY",
+		Email:        req.Email,
+		OrderItems:   ois,
+	}
+
+	if req.Address != nil {
+		ZipCodeInt, err := strconv.Atoi(req.Address.ZipCode)
+		if err != nil {
+			return nil, kerrors.NewBizStatusError(400, "invalid zip code")
+		}
+		placeOrderReq.Address = &order.Address{
+			StreetAddress: req.Address.StreetAddress,
+			City:          req.Address.City,
+			State:         req.Address.State,
+			Country:       req.Address.Country,
+			ZipCode:       int32(ZipCodeInt),
+		}
+	}
+
+	placeOrderResult, err := rpc.OrderClient.PlaceOrder(s.ctx, &placeOrderReq)
+	if err != nil {
+		return nil, err
+	}
+
+	orderId := placeOrderResult.Order.OrderId
+	paymentReq := payment.ChargeReq{
+		Amount:     amount,
+		CreditCard: req.CreditCard,
+		OrderId:    orderId,
+		UserId:     uint32(payload.UserId),
+	}
+
+	paymentResult, err := rpc.PaymentClient.Charge(s.ctx, &paymentReq)
+	if err != nil {
+		return nil, err
+	}
+
+	resp = &checkout.CheckoutResp{
+		OrderId: orderId,
+		TransactionId: paymentResult.TransactionId,
+	}
+
+	return resp, nil
 }
--- app/checkout/infra/rpc/client.go
+++ app/checkout/infra/rpc/client.go
@@ -9,14 +9,18 @@ import (
 	"github.com/whlxbd/gomall/app/checkout/conf"
 	"github.com/whlxbd/gomall/common/clientsuite"
 	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/auth/authservice"
-	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/product/productcatalogservice"
+	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/cart/cartservice"
 	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/order/orderservice"
+	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/payment/paymentservice"
+	"github.com/whlxbd/gomall/rpc_gen/kitex_gen/product/productcatalogservice"
 )
 
 var (
 	ProductClient productcatalogservice.Client
 	AuthClient    authservice.Client
 	OrderClient   orderservice.Client
+	PaymentClient paymentservice.Client
+	CartClient    cartservice.Client
 	once          sync.Once
 	err           error
 )
@@ -25,6 +29,9 @@ func InitClient() {
 	once.Do(func() {
 		initProductClient()
 		initAuthClient()
+		initOrderClient()
+		initPaymentClient()
+		initCartClient()
 	})
 }
 
@@ -65,6 +72,34 @@ func initOrderClient() {
 	}
 
 	OrderClient, err = orderservice.NewClient("order", opts...)
+	if err != nil {
+		klog.Errorf("failed to init client: %v", err)
+	}
+}
+
+func initPaymentClient() {
+	opts := []client.Option{
+		client.WithSuite(clientsuite.CommonGrpcClientSuite{
+			CurrentServiceName: conf.GetConf().Kitex.Service,
+			RegistryAddr:       conf.GetConf().Registry.RegistryAddress[0],
+		}),
+	}
+
+	PaymentClient, err = paymentservice.NewClient("payment", opts...)
+	if err != nil {
+		klog.Errorf("failed to init client: %v", err)
+	}
+}
+
+func initCartClient() {
+	opts := []client.Option{
+		client.WithSuite(clientsuite.CommonGrpcClientSuite{
+			CurrentServiceName: conf.GetConf().Kitex.Service,
+			RegistryAddr:       conf.GetConf().Registry.RegistryAddress[0],
+		}),
+	}
+
+	CartClient, err = cartservice.NewClient("cart", opts...)
 	if err != nil {
 		klog.Errorf("failed to init client: %v", err)
 	}
