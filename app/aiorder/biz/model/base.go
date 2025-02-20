6f0b352835a057e0d7e6d12aaa4ca97f39d466e0
10yihang <15700041128@163.com>
2025-02-20 23:09:21 +0800

    fix: 修复Base结构体中ID字段缺少gorm主键自增标签导致的数据库主键冲突问题

--- app/aiorder/biz/model/base.go
+++ app/aiorder/biz/model/base.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Base struct {
-	ID        string    `json:"id"`
+	ID        string    `gorm:"primarykey;autoIncrement" json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
